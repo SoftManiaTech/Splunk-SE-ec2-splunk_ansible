0474b85c1f315c9070db2ef6b69bca13170ac1c6
Kaliyappan1 <yappank17@gmail.com>
2025-07-02 14:34:45 +0530

    updated s3 bucket

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_key_pair" "generated_key_pair" {
 
 # Upload PEM to S3
 resource "aws_s3_object" "upload_pem_key" {
-  bucket  = "splunk-deployment-test"
+  bucket  = "splunk-deployment-prod"
   key     = "${var.usermail}/keys/${local.final_key_name}.pem"
   content = tls_private_key.generated_key.private_key_pem
 }
